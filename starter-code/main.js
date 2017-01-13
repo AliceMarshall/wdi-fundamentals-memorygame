254e6804e964677c4c012fb40997f44fcf0e1702
AliceMarshall <marshall.alice92@gmail.com>
2017-01-13 15:21:11 +0000

    created alert

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,8 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
+if (cardOne === cardThree || cardTwo === cardFour) {
+	console.log(alert("You've found a match!"));
+} else {
+	console.log(alert("Sorry, try again."));
+}
