08b617b1c98b5731aeb348ddfaf50c8a93a4794b
AliceMarshall <marshall.alice92@gmail.com>
2017-01-17 21:38:36 +0000

    created cards with JavaScript

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,8 +5,19 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
-if (cardOne === cardThree || cardTwo === cardFour) {
-	console.log(alert("You've found a match!"));
-} else {
-	console.log(alert("Sorry, try again."));
-}
+// if (cardOne === cardThree || cardTwo === cardFour) {
+// 	console.log(alert("You've found a match!"));
+// } else {
+// 	console.log(alert("Sorry, try again."));
+// }
+
+var newBoard = document.getElementById('game-board')
+
+var createCards = function() {
+	for (var i=0; i < 4; i++) {
+		var newCards = document.createElement('div');
+		newCards.className = 'card';
+		newBoard.appendChild(newCards);
+	}
+};
+createCards();
